d9806beebd09d34982c232d1d63916fa5c7c6d61
davidfw2uiuc <davidbf2@illinois.edu>
2018-02-03 13:52:38 -0600

    Created Drive control file, moved maunal drive function to drive control.c, included drive control.c in main

---
+++ Mobile Control.c
@@ -0,0 +1,27 @@
+//MOBILE GOAL INTAKE FUNCTIONS
+void mobileOut(int speed) {
+	motor[mobileRight] = speed;
+	motor[mobileLeft] = speed;
+}
+
+void mobileIn(int speed) {
+	motor[mobileRight] = 0 - speed;
+	motor[mobileLeft] = 0 - speed;
+}
+
+void mobileStop() {
+	motor[mobileRight] = 0;
+	motor[mobileLeft] = 0;
+}
+
+void manualMobile() {
+	if (vexRT[Btn7U] == 1) {
+		mobileOut(100);
+	}
+	else if (vexRT[Btn7D] == 1) {
+		mobileIn(100);
+	}
+	else {
+		mobileStop();
+	}
+}
