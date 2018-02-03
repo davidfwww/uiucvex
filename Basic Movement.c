fc2f7a75231dc0cd632acd5b1277ad8a4b26e5fe
davidfw2uiuc <davidbf2@illinois.edu>
2018-02-03 13:28:19 -0600

    Made small changes last week, forgot to commit

--- Basic Movement.c
+++ Basic Movement.c
@@ -6,34 +6,6 @@ void manualDrive() {
 	motor[leftDrive2] = vexRT[Ch3] + vexRT[Ch1];
 }
 
-//INTAKE FUNCTIONS
-void intakeUp(int speed) {
-	motor[coneRight] = speed;
-	motor[coneLeft] = speed;
-}
-
-void intakeDown(int speed) {
-	motor[coneRight] = 0 - speed;
-	motor[coneLeft] = 0 - speed;
-}
-
-void intakeStop() {
-	motor[coneRight] = 0;
-	motor[coneLeft] = 0;
-}
-
-void manualIntake() {
-	if (vexRT[Btn5U] == 1) {
-		intakeUp(100);
-	}
-	else if (vexRT[Btn5D] == 1) {
-		intakeDown(100);
-	}
-	else {
-		intakeStop();
-	}
-}
-
 //MOBILE GOAL INTAKE FUNCTIONS
 void mobileOut(int speed) {
 	motor[mobileRight] = speed;
